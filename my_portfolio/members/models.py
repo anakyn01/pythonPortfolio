1d1b30a88fa42a719c2a85911d286f7e1d2e5f63
Your Name <you@example.com>
2025-05-30 17:26:44 +0900

    model 해결

--- my_portfolio/members/models.py
+++ my_portfolio/members/models.py
@@ -3,8 +3,6 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-    phone = models.IntegerField()
-    joined_date = models.DateField()
 #데이터베이스를 통틀어서 모델이라고 합니다
 #최근 개발에 트랜드는 MVC패턴 Model View Controller mvc => java spring
 #모델은 뭔가요? 데이터가 모델이라는 Object
