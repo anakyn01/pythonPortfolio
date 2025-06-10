9b92d4e16dd1cc23ee4b67d314e4432f92ea79f1
Your Name <you@example.com>
2025-06-10 13:05:37 +0900

    4교시

--- my_portfolio/members/admin.py
+++ my_portfolio/members/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Member #add
 
 # Register your models here.
+# add
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("lastname","firstname","joined_date",)
+
+admin.site.register(Member, MemberAdmin) #add
